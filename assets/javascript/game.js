8deab1ff08534f96f1ae318a0f4de36628a38d67
Shelby Smith <shelbysmith@Shelbys-MacBook-Pro.local>
2018-02-17 16:41:44 -0600

    First commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Global Variables
+var crystalOne = 0;
+var crystalTwo = 0;
+var crystalThree = 0;
+var crystalFour = 0;
+var numberToGuess = 0;
+var playerTotal = 0;
+var computerGuess = 0;
+var wins = 0;
+var losses = 0;
+
+
+//Functions
+
+//Game logic
+
