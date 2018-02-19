f42771a6aa4d76b9f944326dbbde0711e1c355ce
Shelby Smith <shelbysmith@Shelbys-MacBook-Pro.local>
2018-02-19 12:10:18 -0600

    Changed theme to tetris, game fully functional, needs more styles and insturctions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-//Global Variables
+
+
 var crystalOne = 0;
 var crystalTwo = 0;
 var crystalThree = 0;
@@ -8,121 +9,64 @@ var computerGuess;
 var wins = 0;
 var losses = 0;
 
+generateNumbers();
 
+$("#computerGuess").text(computerGuess);
 
-//When the game starts, the computer generates a number beween 19- 120. Also, random numbers should be gererated for all 4 crystals, between 1-12. These numbers should remain constant for the length of the game.
+$(".crystal").on("click", function () {
+   switch (this.id){
 
-//When a player clicks a crystal, the number assigned to that crystal should be added to player total. Player total should be compared to computerguess. if it is equal less than, keep playing, equal to win, and greater than lose
+           case 'crystalOne': playerTotal = crystalOne + playerTotal;
+           checkWin();
+           break;
+       
+           case 'crystalTwo': playerTotal = crystalTwo + playerTotal;
+           checkWin();
+           break;
 
-//Functions
-function startGame(){
+           case 'crystalThree': playerTotal = crystalThree + playerTotal;
+           checkWin();
+           break;
+
+           case 'crystalFour': playerTotal = crystalFour+ playerTotal;
+           checkWin();
+           break;
+}
+});
+
+if (computerGuess == playerTotal){
+    alert("win!")
+} else if (computerGuess < playerTotal){
+    alert("lose!");
+}
+
+function generateNumbers(){
     crystalOne = Math.floor(Math.random()*12 + 1);
     crystalTwo = Math.floor(Math.random()*12 + 1);
     crystalThree = Math.floor(Math.random()*12 + 1);
     crystalFour = Math.floor(Math.random()*12 + 1);
-    computerGuess = Math.floor(Math.random()*119 + 1);
-    $("#computerGuess").html=(computerGuess);
+    computerGuess = Math.floor(Math.random()*100 + 19);
+    }
+
+function resetGame(){
+    generateNumbers();
     playerTotal = 0;
-    if (computerGuess === playerTotal){
-        alert("win");
-        } else if (computerGuess < playerTotal){
-            alert("lose");
-        }
-
-
-    //tests
-    console.log(crystalOne)
-    console.log(crystalTwo)
-    console.log(crystalThree)
-    console.log(crystalFour)
-    console.log(computerGuess)
-    
-
-
-
-// function playerGuess(){
     $("#computerGuess").text(computerGuess);
-    // $("#wins").text(wins);
-    // $("#losses").text(losses);
-    $("#crystalOne").on("click", function() {
-        playerTotal = crystalOne + playerTotal;
-        console.log("pt?" + playerTotal);
-        $("#totalScore").text(playerTotal);
-        if (computerGuess == playerTotal){
-            alert("win");
-            wins++;
-            $("#wins").text("Wins: " + wins);
-            startGame();
-            } else if (computerGuess < playerTotal){
-                alert("lose");
-                losses++;
-                $("#losses").text("Losses: " + losses);
-                startGame();
-            }
-    });
-        $("#crystalTwo").on("click", function() {
-        playerTotal = crystalTwo + playerTotal;
-        console.log("pt?" + playerTotal);
-        $("#totalScore").text(playerTotal);
-        if (computerGuess == playerTotal){
-            alert("win");
-            } else if (computerGuess < playerTotal){
-                alert("lose");
-            }
-    });
-        $("#crystalThree").on("click", function() {
-        playerTotal = crystalThree + playerTotal;
-        console.log("pt?" + playerTotal);
-        $("#totalScore").text(playerTotal);
-        if (computerGuess == playerTotal){
-            alert("win");
-            } else if (computerGuess < playerTotal){
-                alert("lose");
-            }
-    });
-        $("#crystalFour").on("click", function() {
-        playerTotal = crystalFour + playerTotal;
-        console.log("pt?" + playerTotal);
-        $("#totalScore").text(playerTotal);
-        if (computerGuess == playerTotal){
-            alert("win");
-            } else if (computerGuess < playerTotal){
-                alert("lose");
-            }
-   });
-//    if (computerGuess == playerTotal){
-//     alert("win");
-//     } else if (computerGuess < playerTotal){
-//         alert("lose");
-    // }
+    $("#playerScore").text(playerTotal);
 }
 
-// }
-
-//Game logic
-startGame();
-// playerGuess();
-
-
-
-
-
-// if (playerTotal == computerGuess){
-//     alert("win");
-//     } else if (playerTotal > computerGuess){
-//         alert("lose");
-//     }
-
-
-
-
- //     if($("#crystalOne").on("click"), function()){
-    //     playerTotal = crystalOne + playerTotal;
-    //     console.log("pt?" + playerTotal)
-    // } else if (crystalTwo = true){
-    //     playerTotal = crystalTwo + playerTotal;
-    // } else if (crystalThree){
-    //     playerTotal = crystalThree + playerTotal;
-    // } else if (crystalFour){
-    //     playerTotal = crystalFour + playerTotal;
-    // }
+function checkWin(){
+    if (computerGuess == playerTotal){
+        alert("win!");
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        resetGame();
+    } else if (computerGuess < playerTotal){
+        alert("lose!");
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        resetGame();
+    } else {
+        $("#playerScore").text(playerTotal);
+    }
+}
